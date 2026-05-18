367b71b173a1af4b0d743f248410eac7c3341e58
Sekitsuka-Koki-TP <282409224+Sekitsuka-Koki-TP@users.noreply.github.com>
2026-05-18 13:55:58 +0900

    共通定数のヘッダーファイルを追加

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MIN_PRODUCT_ID (1)  // 商品IDの最小値
+#define MAX_PRODUCT_ID (12) // 商品IDの最大値
+
+#define MAX_TYPES_ID (50)   // 商品タイプの最大値
+
+#define EXITCODE (0)        // 入力などの終了コード
+
+#endif // CONSTANTS_H
