9fe0f46fb0b3306646d378f5a104bbb2c2063115
Kaishu-Hoshiba-TP <282409105+Kaishu-Hoshiba-TP@users.noreply.github.com>
2026-05-18 16:49:11 +0900

    add write.c

--- src/write.c
+++ src/write.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "codecapi.h"
+#include "constants.h"
 
 //VendingMachine(種類ID,商品ID,料金,在庫数,合計金額), 購入数,合計金額
-void save_history(VendingMachine vm, int purchaseCount, int totalAmount) {
+void save_history(struct VendingMachine vm, int purchaseCount, int totalAmount) {
     FILE *file = fopen("history.txt", "a"); // 追記モードでファイルを開く
     if (file == NULL) {
         perror("ファイルを開く際にエラーが発生しました");
