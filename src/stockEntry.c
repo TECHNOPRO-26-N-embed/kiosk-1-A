b07176662671b8dc8a68118321986a2e07eeface
Sekitsuka-Koki-TP <282409224+Sekitsuka-Koki-TP@users.noreply.github.com>
2026-05-19 11:21:52 +0900

    商品管理クラスを更新

--- src/stockEntry.c
+++ src/stockEntry.c
@@ -7,7 +7,7 @@ int loop(struct VendingMachine *vm) {
 
         // 商品IDの入力
         int scanNum;
-        printf("商品IDを入力してください (%d-%d)(終了するには%dを入力): ", MIN_PRODUCT_ID, MAX_PRODUCT_ID, EXITCODE);
+        printf("編集をしたい商品IDを入力してください (%d-%d)(終了するには%dを入力): ", MIN_PRODUCT_ID, MAX_PRODUCT_ID, EXITCODE);
         scanf("%d", &scanNum);
         if(scanNum == EXITCODE) return 0;
 
@@ -17,10 +17,12 @@ int loop(struct VendingMachine *vm) {
         }
 
         // 商品IDが有効な場合の処理
-        printf("商品ID: %d の在庫数を入力してください: ", scanNum);
+        printf("商品ID: %d の在庫数を入力してください (現在の在庫数: %d): ", scanNum, vm[scanNum - 1].stock);
         scanf("%d", &vm[scanNum - 1].stock);
-        printf("商品ID: %d の価格を入力してください: ", scanNum);
+        printf("商品ID: %d の価格を入力してください (現在の価格: %d): ", scanNum, vm[scanNum - 1].price);
         scanf("%d", &vm[scanNum - 1].price);
+        printf("商品ID: %d の種類を入力してください (現在の種類: %d): ", scanNum, vm[scanNum - 1].typeID);
+        scanf("%d", &vm[scanNum - 1].typeID);
     }
 
     return 1;
