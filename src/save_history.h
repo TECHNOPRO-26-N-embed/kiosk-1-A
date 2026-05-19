735757c12465b15afc6926a268596de92529f956
Kaishu-Hoshiba-TP <282409105+Kaishu-Hoshiba-TP@users.noreply.github.com>
2026-05-19 10:44:03 +0900

    add save_history.h

---
+++ src/save_history.h
@@ -0,0 +1,8 @@
+#ifndef SAVE_HISTORY_H
+#define SAVE_HISTORY_H
+
+#include "constants.h"
+
+void save_history(struct VendingMachine vm, int purchaseCount, int totalAmount);
+
+#endif // SAVE_HISTORY_H
